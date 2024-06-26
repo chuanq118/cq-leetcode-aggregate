f66e833c580f19267e3f1ed3ab0de7dabc7aa272
chuanq <chuanq118@foxmail.com>
2024-06-26 20:52:43 +0800

    update

--- src/main/java/cn/lqs/niu_ke/分享一下暑期遇到的手撕/LCR 031. LRU 缓存/Solution.java
+++ src/main/java/cn/lqs/niu_ke/分享一下暑期遇到的手撕/LCR 031. LRU 缓存/Solution.java
@@ -1,19 +1,51 @@
 class LRUCache {
     final Node head;
+    final Node tail;
     final int cap;
     final Map<Integer, Node> map;
     public LRUCache(int capacity) {
         this.cap = capacity;
         this.head = new Node();
+        this.tail = new Node();
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
         this.map = new HashMap<>(this.cap);
     }
     
     public int get(int key) {
-        
+        if(!this.map.containsKey(key)){
+            return -1;
+        }
+        Node cNode = this.map.get(key);
+        cNode.prev.next = cNode.next;
+        cNode.next.prev = cNode.prev;
+        cNode.next =  this.head.next;
+        cNode.prev = this.head;
+        this.head.next = cNode;
+        cNode.next.prev = cNode;
+        return cNode.val;
     }
     
     public void put(int key, int value) {
-
+        if(this.map.containsKey(key)){
+            this.map.get(key).val = value;
+            this.get(key);
+            return;
+        }
+        if(this.map.size() == this.cap){
+            Node lNode = this.tail.prev;
+            this.tail.prev = lNode.prev;
+            lNode.prev.next = this.tail;
+            this.map.remove(lNode.key);
+        }
+        Node nNode = new Node();
+        nNode.key = key;
+        nNode.val = value;
+        nNode.prev = this.head;
+        nNode.next = this.head.next;
+        this.head.next = nNode;
+        nNode.next.prev = nNode;
+        this.map.put(key, nNode);
     }
 
     static class Node {
