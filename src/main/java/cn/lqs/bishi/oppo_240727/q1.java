395760d0bda1b300f2fa563d31260c0f42f50e62
chuanq <chuanq118@foxmail.com>
2024-07-30 21:01:00 +0800

    tmp saving

---
+++ src/main/java/cn/lqs/bishi/oppo_240727/q1.java
@@ -0,0 +1,12 @@
+/**
+ * prev_max = Math.max(a, b, c)
+ * new_max = (x - k) + 1
+ * if new_max >= prev_max => "No"
+ * if new_max < prev_max => "Yes"
+ */
+public class Main {
+    public static void main(String[] args){
+        // ...
+    }
+}
+
