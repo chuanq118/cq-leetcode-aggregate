13071166ff08b1f476e4bbc0e40140ed0d6f6302
chuanq <chuanq118@foxmail.com>
2024-08-09 16:37:02 +0800

    savepoint.

--- src/main/java/cn/lqs/interview/design/单例模式_枚举类实现.java
+++ src/main/java/cn/lqs/interview/design/单例模式_枚举类实现.java
@@ -12,17 +12,11 @@ public enum 单例模式_枚举类实现 {
         return INSTANCE;
     }
 
-    public static void doSomething() {
-        System.out.println("hello1");
-    }
-
     public void doSomething2() {
-        System.out.println("hello2");
+        System.out.println("hello world");
     }
-
 
     public static void main(String[] args) {
-        单例模式_枚举类实现.doSomething();
         单例模式_枚举类实现.getInstance().doSomething2();
     }
 }
---
+++ src/main/java/cn/lqs/interview/design/单例模式_静态内部类.java
@@ -0,0 +1,22 @@
+package cn.lqs.interview.design;
+
+public class 单例模式_静态内部类 {
+
+    // 私有化构造方法
+    private 单例模式_静态内部类(){}
+
+    // 注意 static 类静态方法获取实例
+    public static 单例模式_静态内部类 getInstance() {
+        return 静态内部类.instance;
+    }
+
+    // 内部类保证懒加载
+    private static class 静态内部类 {
+        private final static 单例模式_静态内部类 instance = new 单例模式_静态内部类();
+    }
+
+    public void doSomethingByInstance(){
+        System.out.println("Do Something By Instance.");
+    }
+
+}
